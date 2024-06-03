e93fcf4131d22014ce8f2e1f13a3812f62cee4ca
BhawanMalhi <Bhawanmalhi29@gmail.com>
2024-06-02 23:26:58 -0300

    made some changes to paths etc

---
+++ routes/trips.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+function readTrips() {
+    const tripsData = fs.readFileSync("./data/trips.json");
+    const parsedData = JSON.parse(tripsData);
+    return parsedData;
+  }
+
+  router.get("/", (req, res) => {
+    const trips = readTrips();
+    res.json(trips);
+  });
+
+  function ran() {
+    return Math.floor(Math.random() * 12);
+  }
+  router.get("/:id", (req, res) => {
+    const trips = readTrips();
+    const requestedId = req.params.id;
+    console.log(requestedId);
+    const singleTrip = trips.find(t => t.id === requestedId);
+    if (singleTrip) {
+      res.json(singleTrip);
+    } else {
+      res.status(404).json({ message: 'Trip not found' });
+    }
+    res.send(singleTrip);
+  });
+  
+  
+
+  router.post("/", (req, res) => {
+    const trips = readTrips();
+     const newTrip = {
+      id: uuidv4(),
+      place: req.body.place,
+      country:req.body.country,
+      src: `http://localhost:8080/public-images/image${ran()}.jpg`,
+      expenses: []
+    };
+    trips.push(newTrip);
+    fs.writeFileSync("./data/trips.json", JSON.stringify(trips));
+    res.status(201).send(newTrip);
+  });
+  
+  module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+require("dotenv").config();
+const cors = require("cors");
+const tripsRoute = require("./routes/trips.js");
+
+app.use(cors());
+app.use(express.json());
+app.use("/public-images", express.static("./public/images"));
+app.use ("/travelTab/trips", tripsRoute);
+app.use ("/travelTab/trips/:id", tripsRoute);
+
+
+app.get("/", (req, res) => {
+    res.send("the get request");
+  });
+
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
